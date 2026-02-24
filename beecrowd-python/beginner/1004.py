ad2d36ed9e334fbd26501ed96f4f718b490d3c28
Gabriel Souza Carvalho <gabriel.s.carvalho.ai@gmail.com>
2026-02-24 14:25:16 -0300

    Add problem 1004 - Simple Product (Beginner)

---
+++ beecrowd-python/beginner/1004.py
@@ -0,0 +1,17 @@
+"""
+Beecrowd Problem 1004 - Simple Product
+Category: Beginner
+Calculating the product as requested
+"""
+
+
+# Input
+factor1 = int(input())
+factor2 = int(input())
+
+# Processing
+# PROD is the variable name required by the exercise (Product)
+PROD = factor1 * factor2
+
+# Output
+print(f'PROD = {PROD}')
